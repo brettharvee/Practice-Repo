1748bab7aac17822e35bf76da64759637ac53658
Brett Harvey <x2015lyn@stfx.ca>
2016-03-08 15:56:25 -0400

    First Commit

---
+++ Blank Project To Test GitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blank Project To Test GitHub
+//
+//  Created by Brett Harvey on 2016-03-08.
+//  Copyright © 2016 Brett Harvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
